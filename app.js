0980f61d933be78980fe211adb7d28502a9529de
Filip <68464364+DystopiaPark@users.noreply.github.com>
2023-02-10 16:35:51 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let startBtn = document.getElementById("start-btn");
+let btnDiv = document.getElementById("btn-div");
+
+startBtn.addEventListener("click", () => {
+  startBtn.style.display = "none";
+  btnDiv.style.display = "block";
+});
